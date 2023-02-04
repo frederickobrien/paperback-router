70b05253acbfeb276892bf85c54e0ea0bb202f27
frederickobrien <frederik.obrien@gmail.com>
2023-02-04 11:30:46 +0000

    Move over to Svelte
    Much better!

---
+++ ui/src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+	import Globe from '../components/globe.svelte';
+	import aroundTheWorldInEightyDays from '../data/80-days.json';
+	import tracks from '../data/tracks.json';
+</script>
+
+<header>
+	<h1>Paperback Router</h1>
+	<div><i>Around the World in 80 Days</i> by Jules Verne</div>
+</header>
+
+<Globe bookData={aroundTheWorldInEightyDays} />
+
+<style>
+	header {
+		margin: 1em 0;
+		position: fixed;
+		width: 100%;
+		text-align: center;
+	}
+	h1 {
+		font-size: 2em;
+		font-weight: 400;
+	}
+</style>
