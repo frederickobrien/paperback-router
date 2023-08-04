c251e2361246cebfa000014a09e0a164890118c3
frederickobrien <frederick.obrien@guardian.co.uk>
2023-08-04 12:24:46 +0100

    Generate dataset from sole source of truth /data project

---
+++ data/raw-data.js
@@ -0,0 +1,111 @@
+export const rawData = [
+    {
+        "name": "Tracks",
+        "author": "Robyn Davidson",
+        "year": 1977,
+        "description": "blah",
+        "coverLink": "https://m.media-amazon.com/images/I/81md4xV-y6L.jpg",
+        "bookType": "non-fiction",
+        "gutenbergId": null,
+        "geoJsonFile": "tracks.csv"
+    },
+    {
+        "name": "Around the World in Eighty Days",
+        "author": "Jules Verne",
+        "year": 1873,
+        "description": "Phileas Fogg of London and his newly employed French valet Passepartout attempt to circumnavigate the world in 80 days on a £20,000 wager (roughly £1.6 million today) set by his friends at the Reform Club.",
+        "coverLink": "https://upload.wikimedia.org/wikipedia/commons/8/86/Verne_Tour_du_Monde.jpg",
+        "bookType": "fiction",
+        "gutenbergId": 103,
+        "libriVoxLink": "https://librivox.org/around-the-world-in-eighty-days-by-jules-verne-2/",
+        "geoJsonFile": "around-the-world-in-eighty-days.csv"
+    },
+    {
+        "name": "Don Quixote",
+        "author": "Miguel de Cervantes",
+        "year": 1605,
+        "description": "blah",
+        "coverLink": "https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Title_page_first_edition_Don_Quijote.jpg/1024px-Title_page_first_edition_Don_Quijote.jpg",
+        "bookType": "fiction",
+        "gutenbergId": 996,
+        "libriVoxLink": null,
+        "geoJsonFile": "don-quixote.csv"
+    },
+    {
+        "name": "Journey to the West",
+        "author": "Wu Cheng'en",
+        "year": 1592,
+        "description": "The legendary pilgrimage of the Tang dynasty Buddhist monk Xuanzang, who traveled to the 'Western Regions' (Central Asia and India) to obtain Buddhist sūtras (sacred texts) and returned after many trials and much suffering.",
+        "coverLink": "https://upload.wikimedia.org/wikipedia/commons/6/6a/Evl53201b_pic.jpg?20041108143830",
+        "bookType": "fiction",
+        "gutenbergId": 23962,
+        "libriVoxLink": null,
+        "geoJsonFile": "journey-to-the-west.csv"
+    },
+    {
+        "name": "The Grapes of Wrath",
+        "author": "John Steinbeck",
+        "year": 1939,
+        "description": "Set during the Great Depression, the novel focuses on the Joads, a poor family of tenant farmers driven from their Oklahoma home by drought, economic hardship, agricultural industry changes, and bank foreclosures forcing tenant farmers out of work. Due to their nearly hopeless situation, and in part because they are trapped in the Dust Bowl, the Joads set out for California along with thousands of other 'Okies' seeking jobs, land, dignity, and a future.",
+        "coverLink": "https://upload.wikimedia.org/wikipedia/commons/a/ad/The_Grapes_of_Wrath_%281939_1st_ed_cover%29.jpg",
+        "bookType": "fiction",
+        "gutenbergId": null,
+        "libriVoxLink": null,
+        "geoJsonFile": "the-grapes-of-wrath.csv"
+    },
+    {
+        "name": "The Adventures of Huckleberry Finn",
+        "author": "Mark Twain",
+        "year": 1884,
+        "description": "To escape from his violent and drunken father, a 13-year-old boy from the wrong side of the tracks, Huckleberry Finn, fakes his own death and floats away on a raft down the Mississippi with Jim, a runaway slave.",
+        "coverLink": "https://static01.nyt.com/images/2017/01/22/books/review/22POWERS/22powers-superJumbo.jpg",
+        "bookType": "fiction",
+        "gutenbergId": 76,
+        "libriVoxLink": null,
+        "geoJsonFile": "the-adventures-of-huckleberry-finn.csv"
+    },
+    {
+        "name": "The Alchemist",
+        "author": "Paulo Coelho",
+        "year": 1988,
+        "description": "the mystical story of Santiago, an Andalusian shepherd boy who yearns to travel in search of a worldly treasure.",
+        "coverLink": "https://upload.wikimedia.org/wikipedia/en/9/9c/The_Alchemist_%28novel%29.jpg",
+        "bookType": "fiction",
+        "gutenbergId": null,
+        "libriVoxLink": null,
+        "geoJsonFile": "the-alchemist.csv"
+    },
+    {
+        "name": "The Travels of Marco Polo",
+        "author": "Marco Polo",
+        "year": 1298,
+        "description": "Blah",
+        "coverLink": "https://upload.wikimedia.org/wikipedia/commons/2/29/Marco_Polo%2C_Il_Milione%2C_Chapter_CXXIII_and_CXXIV.jpg",
+        "bookType": "non-fiction",
+        "gutenbergId": 10636,
+        "libriVoxLink": null,
+        "geoJsonFile": "the-travels-of-marco-polo.csv"
+    },
+    {
+        "name": "The Great Railway Bazaar",
+        "author": "Paul Theroux",
+        "year": 1975,
+        "description": "Blah",
+        "coverLink": "https://live.staticflickr.com/7749/18210655955_08926d045e_b.jpg",
+        "bookType": "non-fiction",
+        "gutenbergId": null,
+        "libriVoxLink": null,
+        "geoJsonFile": "the-great-railway-bazaar.csv"
+    },
+    {
+        "name": "Moby-Dick; Or, The Whale",
+        "author": "Herman Melville",
+        "year": 1851,
+        "description": "The sailor Ishmael's narrative of the maniacal quest of Ahab, captain of the whaling ship Pequod, for vengeance against Moby Dick, the giant white sperm whale that bit off his leg on the ship's previous voyage.",
+        "coverLink": "https://upload.wikimedia.org/wikipedia/commons/3/36/Moby-Dick_FE_title_page.jpg",
+        "bookType": "fiction",
+        "gutenbergId": 2701,
+        "libriVoxLink": null,
+        "geoJsonFile": "moby-dick.csv"
+    }
+];
